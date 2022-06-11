6e251ffa250935414e09494c12e6cfc92e978422
Steerpike <imthekoul@gmail.com>
2022-06-11 22:04:10 +0300

    Added loading animation Folder

---
+++ src/Components/Home/LoadinAnimation/LoadingAnimation.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const BookShelfWrapper = styled.div``;
+
+const BookList = styled.ul``;
+
+const Book = styled.li``;
+
+const LoadingAnimation = () => {
+  return (
+    <BookShelfWrapper>
+      <BookList></BookList>
+    </BookShelfWrapper>
+  );
+};
+
+export default LoadingAnimation;
