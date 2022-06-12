54e74eaf1f26c7ab3d36934e214d008ca259b191
Steerpike <imthekoul@gmail.com>
2022-06-12 03:38:14 +0300

    Added dummy display for Shop

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import useBooks from '../Hooks/useBooks';
 import LoadingAnimation from '../Home/LoadinAnimation/LoadingAnimation';
 
@@ -7,32 +8,48 @@ const StyledContainer = styled.div`
   min-height: 200vh;
   min-width: 100vw;
   display: grid;
-  grid-template-columns: 20% 1fr;
-  grid-template-rows: 10vh 1fr;
-  background-color: grey;
+  grid-template-rows: 10vh auto;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, 20rem);
+  background-color: #fff;
+  grid-auto-flow: dense;
   position: relative;
   overflow: hidden;
+  overflow-x: hidden;
 `;
 
 const StyledNavBar = styled.nav`
+  grid-area: 1/2 / 1 /2;
   margin: 0;
   padding: 0;
   position: fixed;
   top: 0;
   height: 10vh;
-  width: 10vw;
-  background-color: red;
-  overflow-x: hidden;
+  width: 100%;
+  background-color: #b63e47;
+`;
+
+const BooksContainer = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
 `;
 
 const Shop = ({ books, status }) => {
   const [ShopBooks, setShopBooks] = useState(books);
   let content = <LoadingAnimation />;
   useBooks(setShopBooks, books);
+  console.log(books);
   if (!status && ShopBooks && ShopBooks.length > 0) {
     content = (
       <StyledContainer>
-        <StyledNavBar />
+        <StyledNavBar>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+        </StyledNavBar>
+        <BooksContainer>
+          {ShopBooks.map(book => (
+            <div>{book.bookAuthor}</div>
+          ))}
+        </BooksContainer>
       </StyledContainer>
     );
   }
